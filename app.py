0380ca7c4e57856788663efa6b6892f9ff4d673e
Aman Singh Bhogal <45331015+AmanSinghBhogal@users.noreply.github.com>
2023-04-05 17:23:07 +0530

    Updated Model Locations

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ emotion_classes = {
     6: "Surprise"}
 
 # Loading Trained Model:
-json_file = open(r"D:\Python\Projects\Facial_Detection_Project\model\model_v2.json", 'r')
+json_file = open(r"model/model_v2.json", 'r')
 
 # Loading model.json file into model
 loaded_model_json = json_file.read()
@@ -30,7 +30,7 @@ json_file.close()
 model = model_from_json(loaded_model_json)
 
 # Loading Weights:
-model.load_weights(r"D:\Python\Projects\Facial_Detection_Project\model\new_model_v2.h5")
+model.load_weights(r"model/new_model_v2.h5")
 
 # Loading Face Cascade
 try: 
@@ -41,6 +41,33 @@ except Exception:
 # Configuring Rtc
 RTC_Configuration = RTCConfiguration({"iceServers": [{"url" : ["stun:stun.l.google.com:19302"]}]})
 
+class EmotionDetector(VideoTransformerBase):
+    def detect(self, frame):
+        # Converting frame into 2 array of RGB format.
+        img = frame.to_ndarray(format = "bgr24")
+
+        #Converting the Captured frame to gray scale:
+        gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces available on camera:
+        num_face = face_detector.detectMultiScale(gray_frame, scaleFactor = 1.3, minNeighbors = 5)
+
+        # Take each fave available on the camera and preprocess it:
+        for (x, y, w, h) in num_face:
+            cv2.rectangle(img, (x,y-50), (x+w, y+h+10), (0,255,0), 4)
+            roi_gray_frame = gray_frame[y:y+h, x: x+w]
+            cropped_img = np.expand_dims(cv2.resize(roi_gray_frame, (48,48), -1), 0)
+
+            #Predict the emotion:
+            if np.sum([roi_gray_frame])!=0:
+                emotion_prediction = model.predict(cropped_img)
+                maxindex = int(np.argmax(emotion_prediction))
+                label_position = (x,y)
+                cv2.putText(frame,emotion_classes[maxindex],label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            else:
+                cv2.putText(img,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        
+        return img
 
 
 st.title("Hello Mausmi")
